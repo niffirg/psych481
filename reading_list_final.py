1caa6449ce700b34ad32eaa47f84c5b3617c42ee
niffirg <uwsagriff@gmail.com>
2017-02-27 08:19:41 -0500

    Update for the end of reading week

---
+++ reading_list_final.py
@@ -0,0 +1,237 @@
+'''
+-----------
+27-Feb-2017
+-----------
+
+> Videos
+- Git & GitHub Crash Course for Beginners
+  https://www.youtube.com/watch?v=SWYqp7iY_Tc
+  32:41
+- Python Django Tutorial - Build A Todo App
+  https://www.youtube.com/watch?v=2yXfUPwlZTw
+  51:01
+- Args and Kwargs - Intermediate Python Programming p.25
+  https://www.youtube.com/watch?v=gZB_ENJD34E
+  11:57
+- Python 3 Programming Tutorial - Frequently asked questions
+  (Covered if __name__ == 'main': aspect of importing modules)
+  https://www.youtube.com/watch?v=hlxCpeb9pbU
+  5:32
+
+5:12
+Fourier Series introduction
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-fourier-series-intro
+
+6:24
+Integral of sin(mt) and cos(mt)
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-integral-of-sinmt-and-cosmt
+
+4:27
+Integral of sine times cosine
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-integral-of-sine-times-cosine
+
+8:14
+Integral of product of sines
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-integral-sine-times-sine
+
+7:14
+Integral of product of cosines
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-integral-cosine-times-cosine
+
+6:48
+First term in a Fourier series
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-first-term-fourier-series
+
+6:07
+Fourier coefficients for cosine terms
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-fourier-coefficients-cosine
+
+6:32
+Fourier coefficients for sine terms
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-fourier-coefficients-sine
+
+10:39
+Finding Fourier coefficients for square wave
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-fourier-coefficients-for-square-wave
+
+3:29
+Visualizing the Fourier expansion of a square wave
+https://www.khanacademy.org/science/electrical-engineering/ee-signals/ee-fourier-series/v/ee-visualize-fourier-series-square-wave
+
+> Online Readings
+
+# Math
+https://en.wikipedia.org/wiki/Spectral_radius
+https://en.wikipedia.org/wiki/Eigenvalues_and_eigenvectors
+https://en.wikipedia.org/wiki/Quadric
+https://en.wikipedia.org/wiki/Laplace_operator
+https://en.wikipedia.org/wiki/Diophantine_approximation
+https://en.wikipedia.org/wiki/Exponential_sum
+https://en.wikipedia.org/wiki/Hearing_the_shape_of_a_drum
+https://en.wikipedia.org/wiki/Discrete_mathematics
+https://en.wikipedia.org/wiki/Combinatorics
+https://en.wikipedia.org/wiki/Computational_geometry
+https://en.wikipedia.org/wiki/Optimal_control_theory
+https://en.wikipedia.org/wiki/Boundary_value_problem
+https://en.wikipedia.org/wiki/Initial_value_problem
+https://en.wikipedia.org/wiki/Lipschitz_continuity
+https://en.wikipedia.org/wiki/Hamiltonian_system
+https://en.wikipedia.org/wiki/Hamiltonian_mechanics
+https://en.wikipedia.org/wiki/Symplectic_manifold
+https://en.wikipedia.org/wiki/Cotangent_bundle
+https://en.wikipedia.org/wiki/Phase_space
+https://en.wikipedia.org/wiki/Logistic_function
+https://en.wikipedia.org/wiki/Error_function
+https://en.wikipedia.org/wiki/Even_and_odd_functions#Odd_functions
+https://en.wikipedia.org/wiki/Hyperbolic_tangent
+https://en.wikipedia.org/wiki/Gudermannian_function
+https://en.wikipedia.org/wiki/Log-linear_model
+https://en.wikipedia.org/wiki/Softmax_function
+https://en.wikipedia.org/wiki/Cross_entropy
+https://en.wikipedia.org/wiki/Kullback%E2%80%93Leibler_divergence
+https://en.wikipedia.org/wiki/Phase_portrait
+https://en.wikipedia.org/wiki/Attractor
+https://en.wikipedia.org/wiki/Surreal_number
+https://en.wikipedia.org/wiki/Dedekind_cut
+https://en.wikipedia.org/wiki/Cantor%E2%80%93Dedekind_axiom
+https://en.wikipedia.org/wiki/Finitary_relation
+https://en.wikipedia.org/wiki/Tarski%27s_axioms
+https://en.wikipedia.org/wiki/Robinson_arithmetic
+https://en.wikipedia.org/wiki/Mathematical_visualization
+https://en.wikipedia.org/wiki/Adjacency_matrix
+https://en.wikipedia.org/wiki/Spectral_graph_theory
+https://en.wikipedia.org/wiki/Matrix_similarity
+https://en.wikipedia.org/wiki/Canonical_form#Linear_algebra
+https://en.wikipedia.org/wiki/Characteristic_polynomial
+https://en.wikipedia.org/wiki/Laplacian_matrix
+https://en.wikipedia.org/wiki/Principle_of_least_action
+https://en.wikipedia.org/wiki/Variational_principle
+https://en.wikipedia.org/wiki/Calculus_of_variations
+https://en.wikipedia.org/wiki/Dirichlet%27s_principle
+https://en.wikipedia.org/wiki/Poisson%27s_equation
+https://en.wikipedia.org/wiki/Curl_(mathematics)
+https://en.wikipedia.org/wiki/Power_law
+[wikipedia] Root mean square
+[wikipedia] Euler's identity
+[wikipedia] Sinc function
+
+# Physics
+https://en.wikipedia.org/wiki/Heat_equation
+https://en.wikipedia.org/wiki/Helmholtz_equation
+https://en.wikipedia.org/wiki/Square-cube_law
+https://en.wikipedia.org/wiki/Diffusion-limited_aggregation
+[wikipedia] Frequency
+[wikipedia] Amplitude
+[wikipedia] Normalized frequency
+[wikipedia] Radian per second
+[wikipedia] Diffusion
+[wikipedia] Non-equilibrium thermodynamics
+
+# Learning / Perception
+[wikipedia] Spaced repetition
+[wikipedia] Forgetting curve
+[wikipedia] Speed learning
+[wikipedia] Rapid automatized naming
+[wikipedia] Speed reading
+[wikipedia] Rapid serial visual presentation
+[wikipedia] Auditory masking: Temporal masking
+[wikipedia] Psychological adaptation
+[wikipedia] Orienting response
+
+
+# Neurobiology
+https://en.wikipedia.org/wiki/Adrenergic_agonist
+https://en.wikipedia.org/wiki/Nucleus_basalis_of_Meynert
+[wikipedia] Neural adaptation
+[wikipedia] Catecholamine
+[wikipedia] Adrenergic receptor
+[wikipedia] Synaptic fatigue
+[wikipedia] Long-term depression
+[wikipedia] Metaplasticity
+https://grey.colorado.edu/CompCogNeuro/index.php/CCNBook/Neuron
+https://grey.colorado.edu/CompCogNeuro/index.php/CCNBook/Sims/Networks/Inhibition
+http://www.scholarpedia.org/article/Balance_of_excitation_and_inhibition
+http://www.scholarpedia.org/article/Neural_inhibition
+
+
+# People
+https://en.wikipedia.org/wiki/Hermann_Weyl
+https://en.wikipedia.org/wiki/Alfred_Tarski
+https://en.wikipedia.org/wiki/Nassim_Nicholas_Taleb
+[wikipedia] Stephen Grossberg
+[wikipedia] Carl Friedrich Gauss
+
+# Signals
+https://en.wikipedia.org/wiki/Fourier_transform
+https://en.wikipedia.org/wiki/Linear_time-invariant_theory
+https://en.wikipedia.org/wiki/Low-pass_filter
+https://en.wikipedia.org/wiki/Stationary_process#Weak_or_wide-sense_stationarity
+https://en.wikipedia.org/wiki/Autocorrelation
+https://en.wikipedia.org/wiki/Pitch_detection_algorithm
+http://www.dosits.org/science/advancedtopics/signallevels/
+https://en.wikipedia.org/wiki/Audio_power
+https://en.wikibooks.org/wiki/Signals_and_Systems/Fourier_Series
+[wikipedia] Nyquist-Shannon sampling theorem
+[wikipedia] White noise
+[wikipedia] Discrete-time Fourier transform
+[wikipedia] Digital signal processing
+[wikipedia] Signal_(elecrtical_engineering)
+[wikipedia] Nyquist frequency
+[wikipedia] Discrete Fourier transform
+[wikipedia] Fast Fourier transform
+[wikipedia] Bin-centres
+[wikipedia] Decibel
+[wikipedia] Logarithmic scale
+
+# Probability
+https://en.wikipedia.org/wiki/Martingale_(probability_theory)
+https://en.wikipedia.org/wiki/Statistical_shape_analysis
+https://en.wikipedia.org/wiki/Bayesian_network
+https://en.wikipedia.org/wiki/Nuisance_variable
+https://en.wikipedia.org/wiki/Multivariate_normal_distribution
+https://en.wikipedia.org/wiki/Black_swan_theory
+https://en.wikipedia.org/wiki/Fat-tailed_distribution
+https://en.wikipedia.org/wiki/Seven_states_of_randomness
+https://en.wikipedia.org/wiki/Mersenne_Twister
+https://en.wikipedia.org/wiki/Mean_field_theory
+https://en.wikipedia.org/wiki/Minimax
+[wikipedia] Probability mass function
+[wikipedia] Probability density function
+[wikipedia] Cumulative distribution function
+[wikipedia] Normalization (statistics)
+[wikipedia] Bayes' Theorem
+[wikipedia] Variational Bayesian methods
+[wikipedia] Taleb distribution
+[wikipedia] Independent and identically distributed random variables
+
+# Neural Networks
+https://en.wikipedia.org/wiki/Extreme_learning_machine
+https://en.wikipedia.org/wiki/Liquid_state_machine
+https://en.wikipedia.org/wiki/Reservoir_computing
+https://en.wikipedia.org/wiki/Artificial_neural_network
+https://en.wikipedia.org/wiki/Self-organizing_map
+https://en.wikipedia.org/wiki/Restricted_Boltzmann_machine
+http://www.scholarpedia.org/article/Recurrent_neural_networks
+[wikipedia] Types of artificial neural networks
+[wikipedia] Long short-term memory
+[wikipedia] Shunting
+[wikipedia] SyNAPSE
+https://grey.colorado.edu/CompCogNeuro/index.php/CCNBook/Networks#kWTA_Approximation_for_Inhibition
+https://grey.colorado.edu/CompCogNeuro/index.php/CCNBook/Networks/Energy_and_Harmony
+http://www.scholarpedia.org/article/FitzHugh-Nagumo_model
+http://www.scholarpedia.org/article/Adaptive_resonance_theory
+http://www.scholarpedia.org/article/Echo_state_network
+
+# Computing / Computer Science
+https://en.wikipedia.org/wiki/Randomized_algorithm
+https://en.wikipedia.org/wiki/Spanning_tree
+https://en.wikipedia.org/wiki/Kruskal%27s_algorithm
+https://en.wikipedia.org/wiki/Bellman_equation
+https://en.wikipedia.org/wiki/Minimalism_(computing)	
+https://en.wikipedia.org/wiki/Optimizing_compiler
+https://en.wikipedia.org/wiki/Computational_anatomy
+https://en.wikipedia.org/wiki/Object_code_optimizer
+https://en.wikipedia.org/wiki/L-system
+https://en.wikipedia.org/wiki/Particle_swarm_optimization
+[wikipedia] Genetic algorithm
+'''
